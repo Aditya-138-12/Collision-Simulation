8e87bef5026af2d7a42ed3ff7b50544388cbf374
Aditya-138-12 <adityasaroha456@gmail.com>
2025-04-07 16:22:04 +0530

    Changes 1. Added information for the Ball position, velocity and mass.

--- sketchBall.js
+++ sketchBall.js
@@ -5,6 +5,11 @@ window.addEventListener('load', function(){
     const canvas3 = document.getElementById('main_div_GravityBall');
     const ctx3 = canvas3.getContext('2d');
 
+    const ballMass = document.getElementById('ballMass');
+    const ballPosition = document.getElementById('ballPos');
+    const ballVelocityY = document.getElementById('ballVelY');
+    const ballVelocityX = document.getElementById('ballVelX');
+
     function setup3(){
         ballA = new ball();
     }
@@ -18,6 +23,11 @@ window.addEventListener('load', function(){
         ballA.gravity3();
         ballA.update3();
         ballA.edges3();
+        
+        ballMass.textContent = Math.floor(ballA.mass*1000)/1000;
+        ballPosition.textContent = Math.floor(ballA.position.y*1000)/1000;
+        ballVelocityY.textContent = Math.floor(ballA.velocity.velY*1000)/1000;
+        //ballVelocityX.textContent = Math.floor(ballA.velocity.velX*1000)/1000;
 
         ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
         draw3();
